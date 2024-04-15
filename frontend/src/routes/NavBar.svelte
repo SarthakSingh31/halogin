11a0a0483727e7082bbdba10a9436b913b2b8193
basit-shafiqq <basitshafiq972@gmail.com>
2024-04-15 19:57:47 +0530

    Redesigned Home Page

--- frontend/src/routes/NavBar.svelte
+++ frontend/src/routes/NavBar.svelte
@@ -1,67 +1,47 @@
-<script lang="ts">
-    import { DarkMode, Heading } from "flowbite-svelte";
-    import {
-        Navbar,
-        NavBrand,
-        NavLi,
-        NavUl,
-        NavHamburger,
-        Button,
-        Input,
-        GradientButton,
-    } from "flowbite-svelte";
-    import { SearchOutline } from "flowbite-svelte-icons";
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, ImagePlaceholder, Skeleton, TextPlaceholder, Button } from 'flowbite-svelte';
     import Logo from "../lib/logo.svg";
-
-    import StreamerLoginComponent from "./StreamerLoginComponent.svelte";
 </script>
 
-<Navbar>
-    <NavBrand href="/">
-        <img src={Logo} class="me-3 h-6 sm:h-9" alt="Halogin Logo" />
-        <span
-            class="self-center whitespace-nowrap text-xl font-semibold text-primary-600"
-        >
-            Halogin
-        </span>
-    </NavBrand>
-    <div class="flex md:order-2">
-        <StreamerLoginComponent />
-        <DarkMode class="border dark:border-gray-800 mr-4 ml-2" />
-        <Button
-            color="none"
-            data-collapse-toggle="mobile-menu-3"
-            aria-controls="mobile-menu-3"
-            aria-expanded="false"
-            class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
-        >
-            <!-- <SearchOutline class="w-5 h-5" /> -->
-        </Button>
-        <div class="hidden relative md:block">
-            <div
-                class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none"
-            >
-                <!-- <SearchOutline class="w-4 h-4" /> -->
-            </div>
-            <!-- <Input id="search-navbar" class="ps-10" placeholder="Search..." /> -->
-        </div>
-        <NavHamburger />
-    </div>
-    <NavUl>
-        <NavLi href="/">Home</NavLi>
-        <NavLi href="/#">About</NavLi>
-        <NavLi href="/#">Contact</NavLi>
-    </NavUl>
+<style>
+    .color{
+       background-color: #ecf0f3;
+    }
+    .nav-item {
+        padding-top: 10px;
+        padding-bottom: 10px; 
+        margin-bottom: 8px; 
+        display: flex;
+        align-items: center;
+    }
+    .button-container {
+        display: flex;
+        align-items: center;
+        gap: 8px; 
+    }
+</style>
 
-    <div class="flex ml-auto space-x-2 mt-1">
-        <GradientButton href="/streamerlogin" outline color="purpleToPink"
-            >Login</GradientButton
-        >
-        <GradientButton
-            href="/streamerregister"
-            outline
-            color="purpleToPink"
-            class="mr-2">Signup</GradientButton
-        >
-    </div>
-</Navbar>
+<div class="relative px-8 ">
+    <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
+        <NavBrand href="/">
+            <img src={Logo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Halogin</span>
+        </NavBrand>
+        <NavHamburger/>
+        <NavUl>
+            <div class="nav-item">
+                <NavLi href="/" active={true}>Find Deals</NavLi>
+            </div>
+            <div class="nav-item">
+                <NavLi href="/about">Find Creators</NavLi>
+            </div>
+            <div class="nav-item">
+                <NavLi href="/docs/components/navbar">About Us</NavLi>
+            </div>
+            <div class="button-container">
+                <Button size="xl" color="green" class="nav-item">Log In</Button>
+                <Button size="xl" color="green" class="nav-item">Sign Up</Button>
+            </div>
+        </NavUl>
+    </Navbar>
+</div>
